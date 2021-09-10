5550eb70bd5a898029c78fde4fa49a526e5ab3be
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-09-10 19:17:30 +0300

    Add db usage and custom implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { identifierModuleUrl } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './shared/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-template';
+
+  constructor(private httpService: HttpService) {}
+
+  public ngOnInit(): void {
+    this.httpService
+      .makeGetRequest('http://localhost:3000/users')
+      .subscribe((response) => console.log('Get users:', response));
+
+    setTimeout(() => {
+      const newId = Math.floor(Math.random() * 1000);
+      const body = {
+        id: newId,
+        name: `Angular Template DB Test ${newId}`,
+        url: 'https://github.com/celikbicak/angular-template',
+      };
+
+      this.httpService
+        .makePostRequest('http://localhost:3000/projects', JSON.stringify(body))
+        .subscribe((response) =>
+          console.log('Post request response:', response)
+        );
+    }, 3000);
+
+    setTimeout(() => {
+      this.httpService
+        .makeGetRequest('http://localhost:3000/projects')
+        .subscribe((response) => console.log('Projects Response:', response));
+    }, 5000);
+  }
 }
