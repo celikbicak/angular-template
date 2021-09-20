885b5d732e431e57d4cc8f4fe2f86c854e2b796e
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2021-09-20 18:48:08 +0300

    Add log interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInterceptor } from './shared/interceptors';
 import { FormattedDatePipe } from './shared/pipes';
 
 @NgModule({
   declarations: [AppComponent, FormattedDatePipe],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: LogInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
