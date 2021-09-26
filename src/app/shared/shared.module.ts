2b10548de436c3d780280394477df1839599da62
Caner BEKTAŞ <canerbektas49@gmail.com>
2021-09-26 21:58:39 +0300

    Add side navigation menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,13 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MaterialModule } from "./material/material.module";
-import { FormattedDatePipe } from "./pipes";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
+import { FormattedDatePipe } from './pipes';
 
 @NgModule({
-    declarations: [
-        FormattedDatePipe
-    ],
-    imports: [
-        CommonModule,
-        MaterialModule,
-        FormsModule,
-    ],
-    exports: [
-        MaterialModule,
-        FormsModule,
-        FormattedDatePipe
-    ],
-    providers: [FormattedDatePipe],
+  declarations: [FormattedDatePipe],
+  imports: [CommonModule, MaterialModule, FormsModule],
+  exports: [MaterialModule, FormsModule, FormattedDatePipe],
+  providers: [FormattedDatePipe],
 })
 export class SharedModule {}
